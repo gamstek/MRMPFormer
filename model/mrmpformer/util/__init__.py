17281e8541d0bb944339d2c26c43de94d80f4ec8
Hulot <hulot0715@gmail.com>
2026-08-13 19:24:09 +0800

    chore(model)：删除 mrmpformer/util 中的 utils 副本
    删除 mrmpformer/util/io.py 和 plot_utils.py（utils/ 的镜像副本，无消费者），更新 util/__init__.py 去掉 io 导入。保留 misc/box_ops/logutil（DETR 模型与 main.py 依赖）。16 个模块 import 链路验证通过。

--- model/mrmpformer/util/__init__.py
+++ model/mrmpformer/util/__init__.py
@@ -1,4 +1,3 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """公共工具模块：文件I/O、BBox运算、通用辅助函数。"""
 from mrmpformer.util.misc import safe_torch_load
-from mrmpformer.util.io import load_features, export_results, time_master, load_images
